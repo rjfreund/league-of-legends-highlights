10f890644d881b4f4f379e1c4d89523770d93946
Robert Freund <rfreund@accuratecontrols.com>
2022-06-02 13:42:19 -0500

    -

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video playsInline autoPlay muted loop>
+        <source src="12-10_NA1-4328321612_01.webm" type="video/webm" />
+      Your browser does not support the video tag.
+      </video>
     </div>
   );
 }
